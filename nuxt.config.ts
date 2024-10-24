4774a88f47c2ed760c188fd721cd75879e8371e2
sunny <star138020@gmail.com>
2024-10-24 16:58:52 +0800

    新增 favicon.ico 調整SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,14 +18,14 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: '統計數據地圖',
+      title: '台灣歷年總統 都幾?',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       meta: [
-        { name: 'description', content: '統計數據地圖' },
-        { name: 'format-detection', content: '台灣統計' },
-        { property: 'og:title', content: '統計數據地圖' },
-        { property: 'og:description', content: '台灣統計' },
+        { name: 'description', content: '台灣歷年總統 都幾?' },
+        { name: 'format-detection', content: '台灣歷年總統票數統計' },
+        { property: 'og:title', content: '台灣歷年總統 都幾?' },
+        { property: 'og:description', content: '台灣歷年總統票數統計' },
       ],
     },
   },
